3449ca0aa20274b02aaab6401891efa54e0d1a52
Developersmz <developersmz12@gmail.com>
2024-09-30 18:00:37 +0200

    Update Models.js

--- src/models/Models.js
+++ src/models/Models.js
@@ -34,14 +34,6 @@ const User = db.sequelize.define('User', {
     }
 })
 
-// User.sync({ alter: true })
-//     .then(() => {
-//         console.log('Banco de dados sincronizado e modelo atualizado.');
-//     })
-//     .catch((err) => {
-//         console.log('Erro ao sincronizar o banco de dados:', err);
-//     });
-
 const Home = db.sequelize.define('Home', {
     phrase: {
         type: DataTypes.STRING,
@@ -170,5 +162,13 @@ User.beforeCreate(async (user) => {
     const hash = await bcrypt.hash(user.password, 10)
     user.password = hash
 })
+
+db.sequelize.sync({ force: true })
+  .then(() => {
+      console.log('Tabelas sincronizadas com sucesso!');
+  })
+  .catch((err) => {
+      console.error('Erro ao sincronizar tabelas:', err);
+  });
 
 module.exports = { User, Home, About, Value, Skill, Service, Project, Testimony }
